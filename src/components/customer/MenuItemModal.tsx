b2c3c5d77c3da8b3163b946f87eab65fd9b3cbea
jmhar3 <jmhar@protonmail.com>
2026-01-29 19:08:56 +1100

    implement basic MenuItemModal layout + design

--- src/components/customer/MenuItemModal.tsx
+++ src/components/customer/MenuItemModal.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
-import { Button, Modal, Stack } from "@mantine/core";
+import { useCounter, useMediaQuery } from "@mantine/hooks";
+import { Button, em, Modal, Stack, Text } from "@mantine/core";
+
+import ModifierSelect from "./ModifierSelect";
 
 import type { MenuItemType, Modifier } from "../../helpers/menu";
 import type { OrderItem } from "../../helpers/cart";
+import IngredientsBadges from "./IngredientsBadges";
 
 interface MenuItemModalProps {
   isOpen: boolean;
@@ -14,10 +18,19 @@ interface MenuItemModalProps {
 function MenuItemModal(props: MenuItemModalProps) {
   const { isOpen, onClose, menuItem, onAddToOrder } = props;
 
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
+
+  const [value, { increment, decrement, reset }] = useCounter(1, { min: 1 });
+
   const [selection, setSelection] = useState<OrderItem>({
     menuItem: menuItem,
     totalPrice: menuItem.price,
   });
+
+  const onModalClose = () => {
+    reset();
+    onClose();
+  };
 
   const selectModifier = (modifier: Modifier) => {
     setSelection((prevSelection) => ({
@@ -45,95 +58,66 @@ function MenuItemModal(props: MenuItemModalProps) {
 
   return (
     <Modal
+      fullScreen
+      radius={0}
       opened={isOpen}
-      onClose={onClose}
+      onClose={onModalClose}
       title={menuItem.label.toUpperCase()}
+      transitionProps={{ transition: "fade", duration: 200 }}
+      styles={{
+        header: { background: "cornsilk" },
+        content: { background: "cornsilk" },
+      }}
     >
-      <Stack>
-        <Stack gap={1} p={2} pt={7} pb={20}>
-          {/*{menuItem.modifiers && (
-            <FormControl>
-              <FormLabel>Customise</FormLabel>
-              <FormGroup>
-                <Stack>
-                  {menuItem.modifiers.map((modifier) => (
-                    <FormControlLabel
-                      key={modifier.label + menuItem.label}
-                      sx={{ justify: "space-between" }}
-                      labelPlacement="start"
-                      control={
-                        <Checkbox
-                          onChange={(_event, isChecked) =>
-                            onCheckboxChange(modifier, isChecked)
-                          }
-                        />
-                      }
-                      label={modifier.label}
-                    />
-                  ))}
-                </Stack>
-              </FormGroup>
-            </FormControl>
-          )}
+      <Stack gap="sm" p={2} pt={7} pb={20} align="center">
+        {menuItem.ingredients && (
+          <IngredientsBadges ingredients={menuItem.ingredients} />
+        )}
 
-          {menuItem.modifierCategories?.map((modifierCategory) => (
-            <FormControl key={modifierCategory.label + menuItem.label}>
-              <FormLabel id={modifierCategory.label}>
-                {modifierCategory.label}
-              </FormLabel>
+        {menuItem.modifiers && (
+          <ModifierSelect
+            label="Customise"
+            allowMultipleSelections
+            modifiers={menuItem.modifiers}
+            onModifierSelect={(selectedModifier) => selectedModifier}
+          />
+        )}
 
-              {modifierCategory.allowMultipleSelections ? (
-                <FormGroup>
-                  <Stack>
-                    {modifierCategory.modifiers.map((modifier) => (
-                      <FormControlLabel
-                        key={
-                          modifier.label +
-                          modifierCategory.label +
-                          menuItem.label
-                        }
-                        sx={{ justifyContent: "space-between" }}
-                        labelPlacement="start"
-                        control={<Checkbox />}
-                        label={modifier.label}
-                      />
-                    ))}
-                  </Stack>
-                </FormGroup>
-              ) : (
-                <RadioGroup
-                  row
-                  aria-labelledby={modifierCategory.label}
-                  name={modifierCategory.label}
-                >
-                  <Stack width="100%">
-                    {modifierCategory.modifiers.map((modifier) => (
-                      <FormControlLabel
-                        key={
-                          modifier.label +
-                          modifierCategory.label +
-                          menuItem.label
-                        }
-                        defaultChecked={
-                          modifier.id === modifierCategory.defaultSelected
-                        }
-                        sx={{ justifyContent: "space-between" }}
-                        labelPlacement="start"
-                        value={modifier.label}
-                        control={<Radio />}
-                        label={modifier.label}
-                      />
-                    ))}
-                  </Stack>
-                </RadioGroup>
-              )}
-            </FormControl>
-          ))}*/}
+        {menuItem.modifierCategories?.map((modifierCategory) => (
+          <ModifierSelect
+            key={modifierCategory.label}
+            onModifierSelect={(selectedModifier) => selectedModifier}
+            {...modifierCategory}
+          />
+        ))}
 
-          <Button variant="contained" onClick={() => onAddToOrder(selection)}>
-            Add to order
+        <Button.Group>
+          <Button color="dark" variant="light" radius="md" onClick={decrement}>
+            -
           </Button>
-        </Stack>
+          <Button.GroupSection color="dark" variant="light">
+            {value}
+          </Button.GroupSection>
+          <Button color="dark" variant="light" radius="md" onClick={increment}>
+            +
+          </Button>
+        </Button.Group>
+
+        <Button
+          px="lg"
+          fullWidth
+          color="red"
+          variant="light"
+          justify="space-between"
+          size={isMobile ? "md" : "lg"}
+          rightSection={<Text fw={700}>${selection.totalPrice}</Text>}
+          onClick={() => {
+            onAddToOrder(selection);
+            onModalClose();
+          }}
+        >
+          Add to order
+        </Button>
       </Stack>
     </Modal>
   );
