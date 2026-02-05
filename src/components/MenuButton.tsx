0addae2d7a4c135b3cb0770764469b1c8ea799f0
jmhar3 <jmhar@protonmail.com>
2026-02-05 17:28:06 +1100

    update colour scheme

---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,34 @@
+import { Button, em } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+
+interface MenuButtonProps {
+  label: string;
+  path: string;
+  image?: string;
+  isExternal?: boolean;
+  textAlign?: CanvasTextAlign;
+}
+
+function MenuButton(props: MenuButtonProps) {
+  const { label, path, isExternal, textAlign } = props;
+
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
+
+  return (
+    <Button
+      px="lg"
+      w="100%"
+      component="a"
+      variant="filled"
+      color="darkslategray"
+      justify={textAlign}
+      size={isMobile ? "lg" : "xl"}
+      target={isExternal ? "_blank" : undefined}
+      href={path}
+    >
+      {label}
+    </Button>
+  );
+}
+
+export default MenuButton;
--- src/pages/customer/PageLayout.tsx
+++ src/pages/customer/PageLayout.tsx
@@ -1,73 +1,77 @@
 import { Box, Button, em, Image, Stack, Title } from "@mantine/core";
 import { useLocation } from "react-router-dom";
+import { useMediaQuery } from "@mantine/hooks";
 
 import type { PropsWithChildren } from "react";
 
 import banner from "/assets/cafe-view.jpeg";
-import { useMediaQuery } from "@mantine/hooks";
 import CoffeeIcon from "../../icons/CoffeeIcon";
 
 interface PageLayoutProps extends PropsWithChildren {
-	hideImage?: boolean;
-	image?: string;
-	title?: string;
+  hideImage?: boolean;
+  image?: string;
+  title?: string;
 }
 
 function PageLayout({ children, image, title, hideImage }: PageLayoutProps) {
-	const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-	const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
 
-	const isMenu = pathname === "/";
+  const isMenu = pathname === "/";
 
-	return (
-		<Stack align="center" pb="lg" gap="0">
-			{!isMenu && (
-				<Button
-					href="/"
-					pos="fixed"
-					c="white"
-					component="a"
-					w="fit-content"
-					variant="filled"
-					color="darkslategray"
-					leftSection={<CoffeeIcon />}
-					px={isMobile ? "sm" : "lg"}
-					size={isMobile ? "sm" : "xl"}
-					top={isMobile ? "20px" : "11px"}
-					left={isMobile ? "20px" : "15px"}
-				>
-					Menu
-				</Button>
-			)}
+  return (
+    <Stack align="center" pb="lg" gap="0">
+      {!isMenu && (
+        <Button
+          href="/"
+          pos="fixed"
+          c="white"
+          component="a"
+          w="fit-content"
+          variant="filled"
+          color="darkslategray"
+          leftSection={<CoffeeIcon />}
+          px={isMobile ? "sm" : "lg"}
+          size={isMobile ? "sm" : "xl"}
+          top={isMobile ? "20px" : "11px"}
+          left={isMobile ? "20px" : "15px"}
+        >
+          Menu
+        </Button>
+      )}
 
-			<Box
-				w="100%"
-				pt={isMobile ? "md" : "lg"}
-				pb={isMobile ? "xs" : "md"}
-				pr={isMobile && !isMenu ? "lg" : undefined}
-			>
-				<Title
-					lts="1.6px"
-					ff="Bangers"
-					c="#3f5561"
-					ta={isMobile && !isMenu ? "right" : "center"}
-				>
-					{title || "Middle Child"}
-				</Title>
-			</Box>
+      <Box
+        w="100%"
+        pt={isMobile ? "md" : "lg"}
+        pb={isMobile ? "xs" : "md"}
+        pr={isMobile && !isMenu ? "lg" : undefined}
+      >
+        <Title
+          w="100%"
+          lts="1.6px"
+          ff="Bangers"
+          c="darkslategray"
+          pt={isMobile ? "md" : "lg"}
+          pb={isMobile ? "xs" : "md"}
+          pr={isMobile && !isMenu ? "lg" : undefined}
+          ta={isMobile && !isMenu ? "right" : "center"}
+        >
+          {title || "Middle Child"}
+        </Title>
+      </Box>
 
-			{!hideImage && (
-				<Image
-					fit="cover"
-					h={isMobile ? "190px" : "300px"}
-					src={image || banner}
-				/>
-			)}
+      {!hideImage && (
+        <Image
+          fit="cover"
+          h={isMobile ? "190px" : "300px"}
+          src={image || banner}
+        />
+      )}
 
-			{children}
-		</Stack>
-	);
+      {children}
+    </Stack>
+  );
 }
 
 export default PageLayout;
