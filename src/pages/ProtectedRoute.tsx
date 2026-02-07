5d63586849b8475c0989593ac3a6a905686f01e4
jmhar3 <jmhar@protonmail.com>
2026-02-07 20:10:08 +1100

    create ProtectedRoute wrapper

---
+++ src/pages/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import type { PropsWithChildren } from "react";
+import { Navigate } from "react-router-dom";
+import Loading from "./Loading";
+
+function ProtectedRoute({ children }: PropsWithChildren) {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) return <Loading message="Confirming permissions" />;
+
+  return isAuthenticated ? children : <Navigate to="/" />;
+}
+
+export default ProtectedRoute;
