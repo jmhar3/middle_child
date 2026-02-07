2acf812065fdaba3e59779339132380b9a46913e
jmhar3 <jmhar@protonmail.com>
2026-02-07 16:22:34 +1100

    update LoginButton design

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,11 +1,21 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "@mantine/core";
+import { Button, em } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
+
   return (
-    <Button onClick={() => loginWithRedirect()}>
+    <Button
+      fullWidth
+      bg="white"
+      variant="outline"
+      color="darkslategray"
+      size={isMobile ? "md" : "xl"}
+      onClick={() => loginWithRedirect()}
+    >
       Log In to Earn Loyalty Points
     </Button>
   );
