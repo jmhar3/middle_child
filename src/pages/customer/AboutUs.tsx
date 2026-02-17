a97b295dee591e11e565e741b8f1bcbdaa4bea11
jmhar3 <jmhar@protonmail.com>
2026-02-17 11:53:48 +1100

    update about us info

--- src/pages/customer/AboutUs.tsx
+++ src/pages/customer/AboutUs.tsx
@@ -1,4 +1,4 @@
-import { Text, Box } from "@mantine/core";
+import { Text, Box, Stack } from "@mantine/core";
 import { useLocation } from "react-router-dom";
 
 import PageLayout from "./PageLayout";
@@ -6,18 +6,103 @@ import PageLayout from "./PageLayout";
 import banner from "/assets/cafe-plants.jpg";
 
 function AboutUs() {
-  const aboutUs =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum molestie molestie nisi non mattis. Praesent dapibus nunc vitae justo maximus, id elementum enim accumsan. Quisque tempus viverra metus eu bibendum. Morbi venenatis semper convallis. Praesent placerat convallis aliquet. Vivamus faucibus id orci non fringilla. Maecenas maximus erat vitae eros dapibus dignissim. Aliquam blandit odio nunc, et dapibus urna rutrum et. Ut ac erat nulla.";
-  const aboutUsAi =
-    "AI-powered Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum molestie molestie nisi non mattis. Praesent dapibus nunc vitae justo maximus, id elementum enim accumsan. Quisque tempus viverra metus eu bibendum. Morbi venenatis semper convallis. Praesent placerat convallis aliquet. Vivamus faucibus id orci non fringilla. Maecenas maximus erat vitae eros dapibus dignissim. Aliquam blandit odio nunc, et dapibus urna rutrum et. Ut ac erat nulla.";
-
   const { pathname } = useLocation();
   const isAi = pathname.includes("ai");
 
   return (
     <PageLayout image={banner} title={isAi ? "AI About Us" : "About Us"}>
       <Box p="lg" maw="1440px">
-        <Text>{isAi ? aboutUsAi : aboutUs}</Text>
+        {isAi ? (
+          <Stack>
+            <Text>
+              This is AI generated. It's always been about the coffee. Well at
+              least it has been once I figured out how to make one. We opened
+              back in 2016 with a real intent to do things differently. Hang on
+              a sec,
+            </Text>
+          </Stack>
+        ) : (
+          <Stack>
+            <Text>
+              Ok here we go,
+              <br />
+              It's always been about the coffee. Well at least it has been once
+              I figured out how to make one. We opened back in 2016 with a real
+              intent to do things differently. Hang on a sec,
+            </Text>
+            <Text>
+              What ya after buddy?
+              <br />
+              Big or small?
+              <br />
+              Too easy.
+            </Text>
+            <Text>
+              Yeah, every coffee shop does the same sh*t so I figured let's just
+              not charge surcharges ever and not charge for the milks. It's
+              worked. You guys seem to really appreciate it. Also learnt pretty
+              quick that people want coffee every day of the week. So here we
+              are.
+              <br />
+              Everyday.
+              <br />
+              Maybe not the best time to do this,
+            </Text>
+            <Text>
+              Hey man, shorty and a roll?
+              <br />
+              Spicy or regular?
+              <br />
+              ‘SPICY BACON AND EGG!’
+            </Text>
+            <Text>
+              The food we do is simple and made fresh. The rule is that you can
+              walk around the river with your food in one hand and a coffee in
+              the other. If you need cutlery we don't make it. But if you want
+              to sit in and eat a chicken toastie with a knife and fork go nuts.
+              Be that guy.
+            </Text>
+            <Text>
+              From one coffee and no real clue to 5 different coffees (3
+              rotating black options) from what we genuinely believe are the
+              best roasters in Melbourne. We take making your coffee seriously
+              but try to do it all without pretentiousness (did I spell that
+              properly?). I think we pull it off but I'm maybe not the right
+              person to ask. Hang on,
+            </Text>
+            <Text>
+              Morning buddy,
+              <br />
+              Yeah,
+              <br />
+              Sweet as,
+              <br />
+              Sugar?
+              <br />
+              No syrups sorry,
+              <br />
+              Honey yeah
+              <br />
+              On the way.
+            </Text>
+            <Text>
+              Oh and we have smoothies that are all frozen fresh fruit so they
+              are super cold and we name them after the dogs that visit us
+              everyday. When I was talking about the coffee before did I mention
+              that we sell beans? We sell beans. Heaps. For whatever way you
+              drink your coffee.
+            </Text>
+            <Text>
+              What else? We sell bread and listen to 90's music every Friday.
+            </Text>
+            <Text>
+              Lily is that everything? I think I covered most stuff.
+              <br />
+              I'm not talking about the bathroom. I think they figure we have a
+              bathroom.
+            </Text>
+          </Stack>
+        )}
       </Box>
     </PageLayout>
   );
