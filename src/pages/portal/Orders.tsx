4b5235c9ad385c1d8bffa095cadf0f9cb8bedfbf
jmhar3 <jmhar@protonmail.com>
2026-02-19 13:15:51 +1100

    change protected route method

--- src/pages/portal/Orders.tsx
+++ src/pages/portal/Orders.tsx
@@ -1,21 +1,21 @@
 import { Button, Stack } from "@mantine/core";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
 
-import ProtectedRoute from "../ProtectedRoute";
 import Nav from "../../components/portal/Nav";
 
 function Orders() {
   return (
-    <ProtectedRoute>
-      <Stack>
-        <Nav>
-          <Stack dir="row" gap={2}>
-            <Button variant="outlined">Update Stock</Button>
-            <Button variant="contained">Close Store</Button>
-          </Stack>
-        </Nav>
-      </Stack>
-    </ProtectedRoute>
+    <Stack>
+      <Nav>
+        <Stack dir="row" gap={2}>
+          <Button variant="outlined">Update Stock</Button>
+          <Button variant="contained">Close Store</Button>
+        </Stack>
+      </Nav>
+    </Stack>
   );
 }
 
-export default Orders;
+const ProtectedPortal = withAuthenticationRequired(Orders);
+
+export default ProtectedPortal;
