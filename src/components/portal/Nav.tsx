d447b3afee6952e886a438a5bf7d4e0717162c13
jmhar3 <jmhar@protonmail.com>
2026-02-19 16:51:37 +1100

    style portal Nav

--- src/components/portal/Nav.tsx
+++ src/components/portal/Nav.tsx
@@ -1,18 +1,46 @@
-import { Flex } from "@mantine/core";
+import { Flex, Group } from "@mantine/core";
+import { useLocation } from "react-router-dom";
 
 import NavButton from "../NavButton";
 
 import type { PropsWithChildren } from "react";
 
 function Nav(props: PropsWithChildren) {
+  const { pathname } = useLocation();
+
   return (
-    <Flex gap={4} pos="fixed" justify="space-between">
-      <Flex>
-        <NavButton label="Edit Menu" path="/portal/menu" />
-        <NavButton label="Take Orders" path="/portal/orders" />
-        <NavButton label="View Stats" path="/portal/stats" />
-      </Flex>
-      {props.children}
+    <Flex
+      p="sm"
+      gap="sm"
+      w="100vw"
+      top="0px"
+      pos="fixed"
+      justify="space-between"
+    >
+      <Group gap="sm">
+        <NavButton
+          width="fit-content"
+          label="Take Orders"
+          path="/portal/orders"
+          isDisabled={pathname.includes("orders")}
+        />
+        <NavButton
+          width="fit-content"
+          label="Edit Menu"
+          path="/portal/menu"
+          isDisabled={pathname.includes("menu")}
+        />
+        <NavButton
+          width="fit-content"
+          label="View Stats"
+          path="/portal/stats"
+          isDisabled={pathname.includes("stats")}
+        />
+      </Group>
+
+      <Group gap="sm" w="fit-content">
+        {props.children}
+      </Group>
     </Flex>
   );
 }
