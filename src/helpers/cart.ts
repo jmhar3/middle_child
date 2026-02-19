086f64541be414b2ba1982043062d07b58002b84
jmhar3 <jmhar@protonmail.com>
2026-02-19 16:50:43 +1100

    create hardcoded mock orders

--- src/helpers/cart.ts
+++ src/helpers/cart.ts
@@ -1,6 +1,9 @@
+import dayjs from "dayjs";
+import { menu } from "./menu";
 import type { MenuItemType, Modifier } from "./menu";
 
 export interface User {
+  id: string;
   name: string;
   loyaltyPoints?: number;
 }
@@ -19,16 +22,68 @@ export interface Cart {
   notes?: string;
 }
 
-export interface Order {
+export interface OrderType {
+  id: string;
   user: User;
-  items: OrderItem[];
+  dueAt: dayjs.Dayjs;
   total: number;
-  dueAt: Date;
   notes?: string;
+  items: OrderItem[];
   isAccepted?: boolean;
+  isComplete: boolean;
   isReadyToCollect: boolean;
-  isCollected: boolean;
+  cancellationMessage?: string;
 }
+
+export const mockOrders: OrderType[] = [
+  {
+    id: "1",
+    user: { id: "1", name: "Jess", loyaltyPoints: 3 },
+    dueAt: dayjs().subtract(12, "minutes"),
+    total: 15,
+    notes: "Chuck em in a carry tray pls",
+    items: [
+      {
+        menuItem: menu[0].items[0],
+        modifiers: [{ id: "2", label: "BYO Keep Cup" }],
+        quantity: 2,
+        note: "Extra honey pls :)",
+      },
+    ],
+    isComplete: false,
+    isReadyToCollect: false,
+  },
+  {
+    id: "2",
+    user: { id: "2", name: "Noah", loyaltyPoints: 5 },
+    dueAt: dayjs().add(6, "minutes"),
+    total: 6.5,
+    items: [
+      {
+        menuItem: menu[0].items[2],
+        modifiers: [],
+        quantity: 1,
+      },
+    ],
+    isComplete: false,
+    isReadyToCollect: false,
+  },
+  {
+    id: "3",
+    user: { id: "3", name: "Sam", loyaltyPoints: 4 },
+    dueAt: dayjs().add(9, "minutes"),
+    total: 12,
+    items: [
+      {
+        menuItem: menu[0].items[3],
+        modifiers: [{ id: "2", label: "BYO Keep Cup" }],
+        quantity: 1,
+      },
+    ],
+    isComplete: false,
+    isReadyToCollect: false,
+  },
+];
 
 export const calculateOrderItemPrice = (
   menuItem: MenuItemType,
@@ -57,3 +112,15 @@ export const filterItemFromOrder = (
   existingItems: OrderItem[],
   filterItem: OrderItem,
 ) => existingItems.filter((existingItem) => existingItem !== filterItem);
+
+export const getMinutesFromNow = (targetDate: Date) => {
+  const now = Date.now(); // Current time in milliseconds
+  const targetTime = targetDate.getTime(); // Target date in milliseconds
+
+  const differenceInMilliseconds = targetTime - now;
+  // 1000 milliseconds/second * 60 seconds/minute = 60000 milliseconds/minute
+  const differenceInMinutes = differenceInMilliseconds / 60000;
+
+  // Use Math.floor() or Math.ceil() depending on desired behavior, or leave as is for a precise value
+  return differenceInMinutes;
+};
