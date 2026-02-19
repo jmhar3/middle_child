ddd1c28edc99172feaae45e889c06b4741c19a3d
jmhar3 <jmhar@protonmail.com>
2026-02-19 16:52:37 +1100

    create portal PageLayout

---
+++ src/pages/portal/PageLayout.tsx
@@ -0,0 +1,26 @@
+import { Box, em, Stack } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+
+import Nav from "../../components/portal/Nav";
+
+import type { PropsWithChildren, ReactNode } from "react";
+
+interface PageLayoutProps extends PropsWithChildren {
+  navComponents: ReactNode;
+}
+
+function PageLayout({ children, navComponents }: PageLayoutProps) {
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
+
+  return (
+    <Box>
+      <Nav>{navComponents}</Nav>
+
+      <Stack pt={isMobile ? "4em" : "5.2em"} pb="lg" w="100vw">
+        {children}
+      </Stack>
+    </Box>
+  );
+}
+
+export default PageLayout;
