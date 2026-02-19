b862e3b4964e8c284dafedf9f831e7488bd94c7d
jmhar3 <jmhar@protonmail.com>
2026-02-19 16:52:54 +1100

    mock up portal Orders screen

--- src/pages/portal/Orders.tsx
+++ src/pages/portal/Orders.tsx
@@ -1,21 +1,73 @@
-import { Button, Stack } from "@mantine/core";
-import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { useState } from "react";
+// import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { Group, Stack } from "@mantine/core";
 
-import Nav from "../../components/portal/Nav";
+import PageLayout from "./PageLayout";
+import Order from "../../components/portal/Order";
+import StyledButton from "../../components/StyledButton";
+import ToggleStoreOpen from "../../components/portal/ToggleStoreOpen";
+
+import { orderTimes, store, type OrderTime } from "../../helpers/store";
+import { mockOrders } from "../../helpers/cart";
 
 function Orders() {
+  const [storeInfo, setStoreInfo] = useState(store);
+
+  const onUpdateStockClick = () => {};
+
+  const onSetOrderTime = (orderTime: OrderTime) => {
+    setStoreInfo((prevStore) => ({
+      ...prevStore,
+      currentOrderTime: orderTime,
+    }));
+  };
+
   return (
-    <Stack>
-      <Nav>
-        <Stack dir="row" gap={2}>
-          <Button variant="outlined">Update Stock</Button>
-          <Button variant="contained">Close Store</Button>
-        </Stack>
-      </Nav>
-    </Stack>
+    <PageLayout
+      navComponents={
+        <>
+          <StyledButton
+            variant="outline"
+            label="Update Stock"
+            onClick={onUpdateStockClick}
+          />
+
+          <ToggleStoreOpen
+            isOpen={storeInfo.isOpen}
+            toggleStoreOpen={(isOpen: boolean) =>
+              setStoreInfo((prevStore) => ({ ...prevStore, isOpen }))
+            }
+          />
+        </>
+      }
+    >
+      <Group w="100%" grow p="sm" bg="white">
+        {orderTimes.map((orderTime) => (
+          <StyledButton
+            key={orderTime.label}
+            label={orderTime.label}
+            onClick={() => onSetOrderTime(orderTime)}
+            variant={
+              orderTime === storeInfo.currentOrderTime ? "filled" : "outline"
+            }
+          />
+        ))}
+      </Group>
+
+      <Stack w="100%" px="sm">
+        {mockOrders.map(
+          (order) =>
+            !order.cancellationMessage && (
+              <Order key={order.id} order={order} />
+            ),
+        )}
+      </Stack>
+    </PageLayout>
   );
 }
 
-const ProtectedPortal = withAuthenticationRequired(Orders);
+export default Orders;
 
-export default ProtectedPortal;
+// const ProtectedPortal = withAuthenticationRequired(Orders);
+
+// export default ProtectedPortal;
