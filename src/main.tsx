48c5cf82cfbe003b804eadb05843b8e0da8deac8
jmhar3 <jmhar@protonmail.com>
2026-02-23 15:05:25 +1100

    change analytics import from next to react

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import { Provider } from "react-redux";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import Auth from "./pages/Auth.js";
 import Portal from "./pages/portal/Portal.js";
@@ -24,32 +24,32 @@ import "@mantine/notifications/styles.css";
 import "./global.css";
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<Provider store={store}>
-			<MantineProvider>
-				<Notifications />
-				<Analytics />
-				<BrowserRouter>
-					<Routes>
-						<Route path="*" element={<Auth />} />
+  <StrictMode>
+    <Provider store={store}>
+      <MantineProvider>
+        <Notifications />
+        <Analytics />
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<Auth />} />
 
-						{/* Customer Routes */}
-						<Route index element={<Home />} />
-						<Route path="about-us" element={<AboutUs />} />
-						<Route path="about-us/ai" element={<AboutUs />} />
-						<Route path="partners" element={<Partners />} />
-						<Route path="menu" element={<CustomerMenu />} />
-						<Route path="account" element={<Account />} />
+            {/* Customer Routes */}
+            <Route index element={<Home />} />
+            <Route path="about-us" element={<AboutUs />} />
+            <Route path="about-us/ai" element={<AboutUs />} />
+            <Route path="partners" element={<Partners />} />
+            <Route path="menu" element={<CustomerMenu />} />
+            <Route path="account" element={<Account />} />
 
-						{/* Private Portal */}
-						<Route path="portal">
-							<Route index element={<Navigate to="/portal/orders" />} />
-							<Route path="orders" element={<Portal />} />
-							<Route path="menu" element={<Portal />} />
-						</Route>
-					</Routes>
-				</BrowserRouter>
-			</MantineProvider>
-		</Provider>
-	</StrictMode>,
+            {/* Private Portal */}
+            <Route path="portal">
+              <Route index element={<Navigate to="/portal/orders" />} />
+              <Route path="orders" element={<Portal />} />
+              <Route path="menu" element={<Portal />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </MantineProvider>
+    </Provider>
+  </StrictMode>,
 );
