1302e50b6aaaea44cd977632db39b495ba467c70
jmhar3 <jmhar@protonmail.com>
2026-02-28 15:25:59 +1100

    refactor EditableMenuItem w supplemental changes

--- src/components/portal/EditableMenuItem.tsx
+++ src/components/portal/EditableMenuItem.tsx
@@ -1,20 +1,25 @@
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
+
 import {
+  FileButton,
   Flex,
   Group,
+  Image,
   MultiSelect,
   NumberInput,
   Stack,
   Switch,
-  Text,
   TextInput,
 } from "@mantine/core";
 
+import MenuItemModal from "../MenuItemModal";
 import StyledButton from "../StyledButton";
+import MenuItem from "./MenuItem";
 
-import { modifierCategories } from "../../helpers/menu";
+import { ingredients, modifierCategories, modifiers } from "../../helpers/menu";
 
 import type { MenuItemType } from "../../helpers/menu";
-import { useDisclosure } from "@mantine/hooks";
 
 interface EditableMenuItemProps {
   menuItem: MenuItemType;
@@ -23,79 +28,125 @@ interface EditableMenuItemProps {
 function EditableMenuItem(props: EditableMenuItemProps) {
   const { menuItem } = props;
 
+  const [file, setFile] = useState<File | null>(null);
+  const [editedMenuItem, setEditedMenuItem] = useState<MenuItemType>(menuItem);
+
   const [showItemEdit, { open: openItemEdit, close: closeItemEdit }] =
     useDisclosure(false);
+  const [showItemPreview, { open: openItemPreview, close: closeItemPreview }] =
+    useDisclosure(false);
+
+  const imageUrl = file && URL.createObjectURL(file);
 
   return showItemEdit ? (
-    <Stack p="sm">
-      <Group gap="sm" grow>
-        <TextInput label="Label" value={menuItem.label.toUpperCase()} />
-        <NumberInput label="Price" value={menuItem.price} />
-        <TextInput label="Image" />
-      </Group>
+    <>
+      <MenuItemModal
+        menuItem={editedMenuItem}
+        isOpen={showItemPreview}
+        onClose={closeItemPreview}
+        onAddToOrder={() => {}}
+      />
 
-      <Group gap="sm" grow align="flex-start">
-        <MultiSelect
-          size="md"
-          searchable
-          label="Ingredients"
-          data={modifierCategories.map((category) => ({
-            value: category.label,
-            label: category.label,
-          }))}
-        />
-        <MultiSelect
-          size="md"
-          searchable
-          label="Modifiers"
-          data={modifierCategories.map((category) => ({
-            value: category.label,
-            label: category.label,
-          }))}
-        />
-        <MultiSelect
-          size="md"
-          searchable
-          label="Modifier Categories"
-          defaultValue={["1", "2", "3", "4"]}
-          data={modifierCategories.map((category) => ({
-            value: category.label,
-            label: category.label,
-          }))}
-        />
-      </Group>
+      <Stack p="sm">
+        <Group gap="sm" grow align="flex-end">
+          <TextInput
+            label="Label"
+            value={editedMenuItem.label.toUpperCase()}
+            onChange={(event) =>
+              setEditedMenuItem((prevItem) => ({
+                ...prevItem,
+                label: event.target.value,
+              }))
+            }
+          />
+          <NumberInput
+            label="Price"
+            value={editedMenuItem.price}
+            onChange={(value) =>
+              setEditedMenuItem((prevItem) => ({
+                ...prevItem,
+                price: typeof value === "number" ? value : parseFloat(value),
+              }))
+            }
+          />
+          {file ? (
+            <Flex>
+              <Image src={imageUrl} />
+              <FileButton onChange={setFile} accept="image/png,image/jpeg">
+                {(props) => <StyledButton {...props} label="Upload image" />}
+              </FileButton>
+            </Flex>
+          ) : (
+            <FileButton onChange={setFile} accept="image/png,image/jpeg">
+              {(props) => <StyledButton {...props} label="Upload image" />}
+            </FileButton>
+          )}
+        </Group>
 
-      <Flex justify="space-between">
-        <Group>
-          <Switch
-            checked={menuItem.isInStock}
-            withThumbIndicator={false}
-            label="In Stock"
+        <Group gap="sm" grow align="flex-start">
+          <MultiSelect
+            size="md"
+            searchable
+            label="Ingredients"
+            value={editedMenuItem.ingredients?.map(({ id }) => id)}
+            data={ingredients.map((ingredient) => ({
+              value: ingredient.id,
+              label: ingredient.label,
+            }))}
           />
-          <Switch
-            checked={menuItem.isLoyaltyApplicable}
-            withThumbIndicator={false}
-            label="Loyalty Perk"
+          <MultiSelect
+            size="md"
+            searchable
+            label="Modifiers"
+            value={editedMenuItem.modifiers?.map(({ id }) => id)}
+            data={modifiers.map((modifier) => ({
+              value: modifier.id,
+              label: modifier.label,
+            }))}
           />
-          <Switch
-            checked={menuItem.hasLongPrepTime}
-            withThumbIndicator={false}
-            label="Long Prep Time"
+          <MultiSelect
+            size="md"
+            searchable
+            label="Modifier Categories"
+            value={editedMenuItem.modifierCategories?.map(({ id }) => id)}
+            data={modifierCategories.map((category) => ({
+              value: category.id,
+              label: category.label,
+            }))}
           />
         </Group>
-        <Flex gap="sm" justify="flex-end">
-          <StyledButton label="Preview" variant="outline" onClick={() => {}} />
-          <StyledButton label="Save" onClick={closeItemEdit} />
+
+        <Flex justify="space-between">
+          <Group>
+            <Switch
+              checked={menuItem.isInStock}
+              withThumbIndicator={false}
+              label="In Stock"
+            />
+            <Switch
+              checked={menuItem.isLoyaltyApplicable}
+              withThumbIndicator={false}
+              label="Loyalty Perk"
+            />
+            <Switch
+              checked={menuItem.hasLongPrepTime}
+              withThumbIndicator={false}
+              label="Long Prep Time"
+            />
+          </Group>
+          <Flex gap="sm" justify="flex-end">
+            <StyledButton
+              label="Preview"
+              variant="outline"
+              onClick={openItemPreview}
+            />
+            <StyledButton label="Save" onClick={closeItemEdit} />
+          </Flex>
         </Flex>
-      </Flex>
-    </Stack>
+      </Stack>
+    </>
   ) : (
-    <Flex justify="space-between" p="sm" align="center">
-      <Text fs="1.4em" fw="600">
-        {menuItem.label.toUpperCase()}
-      </Text>
-      <StyledButton label="Edit" onClick={openItemEdit} />
-    </Flex>
+    <MenuItem onEditItemClick={openItemEdit} menuItem={menuItem} />
   );
 }
 
