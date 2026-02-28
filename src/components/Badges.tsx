02dfe64f2ad151db41bc763f9ad1ce1557ff138e
jmhar3 <jmhar@protonmail.com>
2026-02-28 15:58:19 +1100

    update badges to accept edit function + color

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,20 +1,29 @@
-import { Badge, Group, Stack, Text } from "@mantine/core";
+import { ActionIcon, Badge, Flex, Group, Stack, Text } from "@mantine/core";
+import EditIcon from "../icons/EditIcon";
 
 interface BadgesProps {
   label: string;
-  badges: { id: string; label: string }[];
+  badges: { id: string; label: string; color?: string }[];
+  onEditBadges?: () => void;
 }
 
-function Badges({ label, badges }: BadgesProps) {
+function Badges({ label, badges, onEditBadges }: BadgesProps) {
   return (
     <Stack gap="6" w="100%">
-      <Text>{label}</Text>
+      <Flex gap="sm">
+        <Text>{label}</Text>
+        {onEditBadges && (
+          <ActionIcon color="darkslategray" onClick={onEditBadges}>
+            <EditIcon />
+          </ActionIcon>
+        )}
+      </Flex>
 
       <Group gap="6" style={{ overflow: "auto" }}>
         {badges.map((badge) => (
           <Badge
             radius="sm"
-            color="darkslategray"
+            color={badge.color ? badge.color : "darkslategray"}
             variant="filled"
             key={badge.id}
           >
