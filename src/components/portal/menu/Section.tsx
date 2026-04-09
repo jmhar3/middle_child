9b3e05b78b415497420905df5cda842aee789ebd
jmhar3 <jmhar@protonmail.com>
2026-04-09 16:11:12 +1000

    handle deleteSection click

--- src/components/portal/menu/Section.tsx
+++ src/components/portal/menu/Section.tsx
@@ -14,6 +14,9 @@ import type {
   MenuItemType,
   Section as SectionType,
 } from "../../../state/menu/menuSlice";
+import { useAppDispatch } from "../../../state/hooks";
+import { deleteSection } from "../../../state/menu/menuThunks";
+import { notifications } from "@mantine/notifications";
 
 function Section({ section }: { section: SectionType }) {
   const blankMenuItem: MenuItemType = {
@@ -26,6 +29,7 @@ function Section({ section }: { section: SectionType }) {
     order: section.items.length + 1,
   };
 
+  const [isDeletingSection, setIsDeletingSection] = useState(false);
   const [newMenuItem, setNewMenuItem] = useState<MenuItemType | null>(
     section.items.length === 0 ? blankMenuItem : null,
   );
@@ -48,93 +52,123 @@ function Section({ section }: { section: SectionType }) {
     { close: closeConfirmDelete, open: openConfirmDelete },
   ] = useDisclosure(false);
 
+  const dispatch = useAppDispatch();
+
   const onCloseEditableItem = () => {
     closeEditableMenuItem();
     setNewMenuItem(null);
   };
 
   const onDeleteSectionItem = (id: string) => {
+    console.log(id);
     // dispatch(deleteMenuItem(id))
     onCloseEditableItem();
   };
 
+  const onConfirmDeleteSection = () => {
+    setIsDeletingSection(true);
+    closeConfirmDelete();
+    dispatch(deleteSection(section.id))
+      .then(() => {
+        notifications.show({
+          withCloseButton: false,
+          message: `${section.label} successfully deleted`,
+          position: "bottom-right",
+          color: "green",
+        });
+      })
+      .catch((error) =>
+        notifications.show({
+          message: error,
+          withCloseButton: false,
+          position: "bottom-right",
+          color: "red",
+        }),
+      )
+      .finally(() => setIsDeletingSection(true));
+  };
+
   return (
-    <>
-      <ConfirmationModal
-        label={section.label}
-        isOpen={showConfirmDelete}
-        onClose={closeConfirmDelete}
-        onConfirmDelete={closeConfirmDelete}
-      />
-
-      <Stack gap="0">
-        <Stack pb="sm">
-          <Group
-            grow
-            p="sm"
-            gap="sm"
-            w="100%"
-            bdrs="sm"
-            bd="1px solid lightgray"
-            bg="white"
-          >
-            <StyledButton label="Reorder Items" onClick={openReorderDrawer} />
-
-            <StyledButton
-              label="Rename Section"
-              onClick={openUpsertSectionModal}
-            />
-
-            <StyledButton label="Delete Section" onClick={openConfirmDelete} />
-          </Group>
+    <Stack gap="0">
+      <Stack pb="sm">
+        <Group
+          grow
+          p="sm"
+          gap="sm"
+          w="100%"
+          bdrs="sm"
+          bg="white"
+          bd="1px solid lightgray"
+        >
+          <StyledButton label="Reorder Items" onClick={openReorderDrawer} />
 
           <StyledButton
-            label="Add New Menu Item"
-            onClick={() => {
-              openEditableMenuItem();
-              setNewMenuItem(blankMenuItem);
-            }}
+            label="Rename Section"
+            onClick={openUpsertSectionModal}
           />
-        </Stack>
 
-        {newMenuItem && showEditableMenuItem && (
-          <>
-            <EditableItem
-              menuItem={newMenuItem}
-              onCloseEditableItem={onCloseEditableItem}
-              showCancelButton={section.items.length > 0}
-            />
-            <Divider />
-          </>
-        )}
-
-        {section.items.map((menuItem, index) => (
-          <>
-            {index > 0 && <Divider />}
-            <ItemEditPreview
-              menuItem={menuItem}
-              onDeleteItem={onDeleteSectionItem}
-            />
-          </>
-        ))}
-
-        {showUpsertSectionModal && (
-          <UpsertSectionModal
-            isOpen={showUpsertSectionModal}
-            onClose={closeUpsertSectionModal}
+          <StyledButton
+            label="Delete Section"
+            onClick={openConfirmDelete}
+            isLoading={isDeletingSection}
           />
-        )}
+        </Group>
 
-        {showReorderDrawer && (
-          <ReorderDrawer
-            isOpen={showReorderDrawer}
-            onClose={closeReorderDrawer}
-            label="Menu Items"
-            items={section.items}
-          />
-        )}
+        <StyledButton
+          label="Add New Menu Item"
+          onClick={() => {
+            openEditableMenuItem();
+            setNewMenuItem(blankMenuItem);
+          }}
+        />
       </Stack>
-    </>
+
+      {newMenuItem && showEditableMenuItem && (
+        <>
+          <EditableItem
+            menuItem={newMenuItem}
+            onCloseEditableItem={onCloseEditableItem}
+            showCancelButton={section.items.length > 0}
+          />
+          <Divider />
+        </>
+      )}
+
+      {section.items.map((menuItem, index) => (
+        <>
+          {index > 0 && <Divider />}
+          <ItemEditPreview
+            menuItem={menuItem}
+            onDeleteItem={onDeleteSectionItem}
+          />
+        </>
+      ))}
+
+      {showUpsertSectionModal && (
+        <UpsertSectionModal
+          isOpen={showUpsertSectionModal}
+          onClose={closeUpsertSectionModal}
+        />
+      )}
+
+      {showReorderDrawer && (
+        <ReorderDrawer
+          isOpen={showReorderDrawer}
+          onClose={closeReorderDrawer}
+          label="Menu Items"
+          items={section.items}
+        />
+      )}
+
+      {showConfirmDelete && (
+        <ConfirmationModal
+          label={section.label}
+          isOpen={showConfirmDelete}
+          onClose={closeConfirmDelete}
+          onConfirmDelete={onConfirmDeleteSection}
+        />
+      )}
+    </Stack>
   );
 }
 
