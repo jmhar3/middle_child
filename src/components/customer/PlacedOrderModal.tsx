d1e72578fdcfd4eb1f64ed6b2924b0ca6f8137ab
jmhar3 <jmhar@protonmail.com>
2026-05-18 18:13:00 +1000

    create Placed Order Modal

---
+++ src/components/customer/PlacedOrderModal.tsx
@@ -0,0 +1,74 @@
+import { Button, Divider, Modal, Stack, Text } from "@mantine/core";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+import type { Cart } from "../../state/types";
+
+interface PlacedOrderModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  order: Cart;
+}
+
+function PlacedOrderModal(props: PlacedOrderModalProps) {
+  const {
+    isOpen,
+    onClose,
+    order: { items },
+  } = props;
+
+  return (
+    <Modal
+      fullScreen
+      radius={0}
+      title="ORDER SUCCESSFUL"
+      opened={isOpen}
+      onClose={onClose}
+      transitionProps={{ transition: "fade", duration: 200 }}
+      styles={{
+        header: { background: "whitesmoke" },
+        content: { background: "whitesmoke" },
+      }}
+    >
+      <Stack>
+        <Divider w="100%" />
+
+        <Stack gap="xs" align="center">
+          <Text>Thanks for ordering with Middle Child</Text>
+
+          <Text>
+            Your order will be ready at{" "}
+            {dayjs(props.order.pickUpTime).format("h:mm")}
+          </Text>
+        </Stack>
+
+        <Divider w="100%" />
+
+        <Stack gap="xs">
+          {items.map((item, index) => (
+            <>
+              {index > 0 && <Divider w="100%" />}
+
+              <Stack key={item.id} gap="xs">
+                <Text>
+                  {item.quantity} x {item.item.label}
+                </Text>
+                <Text>{item.modifiers?.join(", ")}</Text>
+              </Stack>
+            </>
+          ))}
+        </Stack>
+
+        <Button color="darkslategray" onClick={onClose}>
+          Close
+        </Button>
+      </Stack>
+    </Modal>
+  );
+}
+
+export default PlacedOrderModal;
