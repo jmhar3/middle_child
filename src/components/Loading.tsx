a7e41c43e42d47a1462463e09ba4b08d8e9eb1fd
jmhar3 <jmhar@protonmail.com>
2026-06-09 14:24:27 +1000

    redesign Loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,24 @@
-import { Loader, Stack, Text } from "@mantine/core";
+import { Loader, Modal, Stack, Title } from "@mantine/core";
 
 function Loading({ message }: { message: string }) {
-	return (
-		<Stack align="center" justify="center" pt="xl">
-			<Loader color="darkslategray" />
-			<Text>{message}</Text>
-		</Stack>
-	);
+  return (
+    <Modal
+      size="xs"
+      centered
+      opened={true}
+      withCloseButton={false}
+      onClose={() => {}}
+      overlayProps={{
+        backgroundOpacity: 0.45,
+        blur: 3,
+      }}
+    >
+      <Stack align="center" justify="center" py="lg" gap="lg">
+        <Loader color="darkslategray" />
+        <Title size="1.5em">{message}...</Title>
+      </Stack>
+    </Modal>
+  );
 }
 
 export default Loading;
