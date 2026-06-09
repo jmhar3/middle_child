80b833fdccb46c7d6eecd0b7b5708e76de95c25d
jmhar3 <jmhar@protonmail.com>
2026-06-09 14:25:01 +1000

    format file

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,24 +1,24 @@
 import { Loader, Modal, Stack, Title } from "@mantine/core";
 
 function Loading({ message }: { message: string }) {
-  return (
-    <Modal
-      size="xs"
-      centered
-      opened={true}
-      withCloseButton={false}
-      onClose={() => {}}
-      overlayProps={{
-        backgroundOpacity: 0.45,
-        blur: 3,
-      }}
-    >
-      <Stack align="center" justify="center" py="lg" gap="lg">
-        <Loader color="darkslategray" />
-        <Title size="1.5em">{message}...</Title>
-      </Stack>
-    </Modal>
-  );
+	return (
+		<Modal
+			size="xs"
+			centered
+			opened={true}
+			withCloseButton={false}
+			onClose={() => {}}
+			overlayProps={{
+				backgroundOpacity: 0.45,
+				blur: 3,
+			}}
+		>
+			<Stack align="center" justify="center" py="lg" gap="lg">
+				<Loader color="darkslategray" />
+				<Title size="1.5em">{message}...</Title>
+			</Stack>
+		</Modal>
+	);
 }
 
 export default Loading;
