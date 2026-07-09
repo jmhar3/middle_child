2ef3741934e4f8fe1d979311bbeeef9a7cf28080
jmhar3 <jmhar@protonmail.com>
2026-07-09 13:34:14 +1000

    retrieve + display store opening hours

--- src/pages/customer/Home.tsx
+++ src/pages/customer/Home.tsx
@@ -14,134 +14,152 @@ import MapPinIcon from "../../icons/MapPinIcon";
 import InstagramIcon from "../../icons/InstagramIcon";
 import EmailIcon from "../../icons/EmailIcon";
 import {
-  selectStoreInfoStatus,
-  selectStoreIsOpen,
+	selectStoreInfo,
+	selectStoreInfoStatus,
+	selectStoreIsOpen,
 } from "../../state/storeInfo/storeInfoSlice";
 import { fetchStoreInfo } from "../../state/storeInfo/storeInfoThunks";
 
 function Home() {
-  const isMobile = useMediaQuery("(max-width: 960px)");
+	const isMobile = useMediaQuery("(max-width: 960px)");
 
-  const dispatch = useAppDispatch();
-  const userStatus = useAppSelector(selectUserStatus);
-  const user = useAppSelector(selectUser);
-  const storeInfoStatus = useAppSelector(selectStoreInfoStatus);
-  const storeIsOpen = useAppSelector(selectStoreIsOpen);
+	const dispatch = useAppDispatch();
+	const userStatus = useAppSelector(selectUserStatus);
+	const user = useAppSelector(selectUser);
+	const storeInfoStatus = useAppSelector(selectStoreInfoStatus);
+	const storeInfo = useAppSelector(selectStoreInfo);
+	const storeIsOpen = useAppSelector(selectStoreIsOpen);
 
-  useEffect(() => {
-    if (userStatus === "idle") {
-      dispatch(fetchUser());
-    }
-    if (storeInfoStatus === "idle") {
-      dispatch(fetchStoreInfo());
-    }
-  }, [dispatch, userStatus, storeInfoStatus]);
+	useEffect(() => {
+		if (userStatus === "idle") {
+			dispatch(fetchUser());
+		}
+		if (storeInfoStatus === "idle") {
+			dispatch(fetchStoreInfo());
+		}
+	}, [dispatch, userStatus, storeInfoStatus]);
 
-  return (
-    <PageLayout>
-      <Stack w="100%" gap="3" p="3">
-        <NavButton
-          label={`Order Here for Pick Up ${storeIsOpen ? "" : "from 8am"}`}
-          path="/menu"
-          isDisabled={!storeIsOpen}
-        />
+	console.log(storeInfo);
 
-        <Flex w="100%" gap="3">
-          <NavButton label="About Us" path="/about-us" />
-          <NavButton label="Also About Us (AI)" path="/about-us/ai" />
-        </Flex>
+	return (
+		<PageLayout>
+			<Stack w="100%" gap="3" p="3">
+				<NavButton
+					path="/menu"
+					isDisabled={!storeIsOpen}
+					label="Order Here for Pick Up"
+				/>
 
-        <NavButton label="Look Here & Don't Pick Up" path="/view-menu" />
+				<Flex w="100%" gap="3">
+					<NavButton label="About Us" path="/about-us" />
+					<NavButton label="Also About Us (AI)" path="/about-us/ai" />
+				</Flex>
 
-        <NavButton label="Our Partners" path="/partners" />
+				<NavButton label="Look Here & Don't Pick Up" path="/view-menu" />
 
-        <Flex w="100%" gap="3">
-          <NavButton
-            label="Reviews"
-            textAlign="right"
-            path="https://www.google.com/search?sca_esv=0bd1decbe45b1982&rlz=1C5CHFA_enAU1141AU1151&sxsrf=ANbL-n6dgHEvof21myM1xLGLOgYttYSScg:1769577318013&q=middle+child+cafe&si=AL3DRZEsmMGCryMMFSHJ3StBhOdZ2-6yYkXd_doETEE1OR-qOcsicezydKAKzR4xDjMImAz9FkPYs9pyJVhJLn6zcmohaCYcQtNfHZOq8KOmUC-aefJZkJCQvgvrLoZcUfgGNiC4tf0G&sa=X&ved=2ahUKEwjbjsWbva2SAxV3TmwGHSxWMFUQrrQLegQIGxAA&biw=1512&bih=823&dpr=2&aic=0"
-            isExternal
-          />
-          <NavButton
-            label="Instagram"
-            path="https://www.instagram.com/middlechild_cafe/"
-            textAlign="left"
-            isExternal
-          />
-        </Flex>
+				<NavButton label="Our Partners" path="/partners" />
 
-        <NavButton
-          label={user ? "Manage Account" : "Login / Sign Up"}
-          path="/account"
-        />
+				<Flex w="100%" gap="3">
+					<NavButton
+						label="Reviews"
+						textAlign="right"
+						path="https://www.google.com/search?sca_esv=0bd1decbe45b1982&rlz=1C5CHFA_enAU1141AU1151&sxsrf=ANbL-n6dgHEvof21myM1xLGLOgYttYSScg:1769577318013&q=middle+child+cafe&si=AL3DRZEsmMGCryMMFSHJ3StBhOdZ2-6yYkXd_doETEE1OR-qOcsicezydKAKzR4xDjMImAz9FkPYs9pyJVhJLn6zcmohaCYcQtNfHZOq8KOmUC-aefJZkJCQvgvrLoZcUfgGNiC4tf0G&sa=X&ved=2ahUKEwjbjsWbva2SAxV3TmwGHSxWMFUQrrQLegQIGxAA&biw=1512&bih=823&dpr=2&aic=0"
+						isExternal
+					/>
+					<NavButton
+						label="Instagram"
+						path="https://www.instagram.com/middlechild_cafe/"
+						textAlign="left"
+						isExternal
+					/>
+				</Flex>
 
-        <NavButton label="Nudes" path="" />
+				<NavButton
+					label={user ? "Manage Account" : "Login / Sign Up"}
+					path="/account"
+				/>
 
-        <Stack align="center" gap="sm" pb={isMobile ? undefined : "lg"}>
-          <Flex
-            py="md"
-            align="center"
-            gap={isMobile ? "sm" : "xl"}
-            direction={isMobile ? "column" : "row"}
-          >
-            <Stack align="center" gap="0">
-              <Text>BREWING HOURS</Text>
-              <Flex gap="md">
-                <Stack align={isMobile ? "flex-end" : "flex-start"} gap="0">
-                  <Text>Mon - Fri</Text>
-                  <Text>Sat - Sun</Text>
-                </Stack>
-                <Stack align={isMobile ? "flex-start" : "flex-end"} gap="0">
-                  <Text>7:30am - 1pm</Text>
-                  <Text>7:30am - 2pm</Text>
-                </Stack>
-              </Flex>
-            </Stack>
+				<NavButton label="Nudes" path="" />
 
-            {!isMobile && <Divider orientation="vertical" />}
+				<Stack gap="sm" align="center" pb={isMobile ? undefined : "lg"}>
+					<Flex
+						py="md"
+						align="center"
+						gap={isMobile ? "sm" : "xl"}
+						direction={isMobile ? "column" : "row"}
+					>
+						<Stack align="center" gap="0">
+							<Text>BREWING HOURS</Text>
 
-            <Stack align="center" gap="0">
-              <Text>HOLIDAY HOURS</Text>
-              <Flex gap="md">
-                <Stack align={isMobile ? "flex-end" : "flex-start"} gap="0">
-                  <Text>Christmas Day</Text>
-                  <Text>Jan 26 - Jan 31st</Text>
-                </Stack>
-                <Stack align={isMobile ? "flex-start" : "flex-end"} gap="0">
-                  <Text>8am - Midday</Text>
-                  <Text>8am - 2pm</Text>
-                </Stack>
-              </Flex>
-            </Stack>
-          </Flex>
+							<Flex gap="md">
+								<Stack align={isMobile ? "flex-end" : "flex-start"} gap="0">
+									{storeInfo.opening_hours.map(({ label }) => (
+										<Text key={label}>{label}</Text>
+									))}
+								</Stack>
 
-          <Flex
-            pb="sm"
-            direction={isMobile ? "column" : "row"}
-            gap={isMobile ? "sm" : "md"}
-          >
-            <Link
-              icon={<MapPinIcon />}
-              link="https://www.google.com/maps/place/Middle+Child/data=!4m2!3m1!1s0x0:0x254649be2689f48e?sa=X&ved=1t:2428&ictx=111"
-              label="327 Maribyrnong Rd, Ascot Vale VIC 3032"
-            />
+								<Stack align={isMobile ? "flex-start" : "flex-end"} gap="0">
+									{storeInfo.opening_hours.map(({ label, hours }) => (
+										<Text key={label}>
+											{hours.from} - {hours.to}
+										</Text>
+									))}
+								</Stack>
+							</Flex>
+						</Stack>
 
-            <Link
-              icon={<EmailIcon />}
-              link="mailto:middlechildcafe@gmail.com"
-              label="GOT ANY QUESTIONS? EMAIL US"
-            />
+						{!isMobile && <Divider orientation="vertical" />}
 
-            <Link
-              icon={<InstagramIcon />}
-              link="https://ig.me/m/middlechild_cafe"
-              label="MESSAGE US ON INSTAGRAM"
-            />
-          </Flex>
-        </Stack>
-      </Stack>
-    </PageLayout>
-  );
+						{storeInfo.holiday_opening_hours && (
+							<Stack align="center" gap="0">
+								<Text>HOLIDAY HOURS</Text>
+
+								<Flex gap="md">
+									<Stack align={isMobile ? "flex-end" : "flex-start"} gap="0">
+										{storeInfo.holiday_opening_hours.map(({ label }) => (
+											<Text key={label}>{label}</Text>
+										))}
+									</Stack>
+
+									<Stack align={isMobile ? "flex-start" : "flex-end"} gap="0">
+										{storeInfo.holiday_opening_hours.map(({ label, hours }) => (
+											<Text key={label}>
+												{hours.from} - {hours.to}
+											</Text>
+										))}
+									</Stack>
+								</Flex>
+							</Stack>
+						)}
+					</Flex>
+
+					<Flex
+						pb="sm"
+						direction={isMobile ? "column" : "row"}
+						gap={isMobile ? "sm" : "md"}
+					>
+						<Link
+							icon={<MapPinIcon />}
+							link="https://www.google.com/maps/place/Middle+Child/data=!4m2!3m1!1s0x0:0x254649be2689f48e?sa=X&ved=1t:2428&ictx=111"
+							label="327 Maribyrnong Rd, Ascot Vale VIC 3032"
+						/>
+
+						<Link
+							icon={<EmailIcon />}
+							link="mailto:middlechildcafe@gmail.com"
+							label="GOT ANY QUESTIONS? EMAIL US"
+						/>
+
+						<Link
+							icon={<InstagramIcon />}
+							link="https://ig.me/m/middlechild_cafe"
+							label="MESSAGE US ON INSTAGRAM"
+						/>
+					</Flex>
+				</Stack>
+			</Stack>
+		</PageLayout>
+	);
 }
 
 export default Home;
