ccd32e7e7637768d30ed2e1b399958ef9b3c0ce8
jmhar3 <jmhar@protonmail.com>
2026-07-12 15:10:29 +1000

    log

--- src/components/customer/PaymentHandler.tsx
+++ src/components/customer/PaymentHandler.tsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 
 import {
-	Accordion,
-	Divider,
-	Flex,
-	Loader,
-	Modal,
-	Stack,
-	Text,
+  Accordion,
+  Divider,
+  Flex,
+  Loader,
+  Modal,
+  Stack,
+  Text,
 } from "@mantine/core";
 
 import {
-	GooglePay,
-	CreditCard,
-	PaymentForm,
+  GooglePay,
+  CreditCard,
+  PaymentForm,
 } from "react-square-web-payments-sdk";
 
 import { supabase } from "../../supabase";
@@ -22,113 +22,116 @@ import OrderSummary from "./OrderSummary";
 import type { PendingOrderType } from "../../state/types";
 
 interface PaymentHandlerProps {
-	userId: string;
-	order: PendingOrderType;
-	isOpen: boolean;
-	onClose: () => void;
-	onSuccess: () => void;
-	onFailure: (error?: string) => void;
+  userId: string;
+  order: PendingOrderType;
+  isOpen: boolean;
+  onClose: () => void;
+  onSuccess: () => void;
+  onFailure: (error?: string) => void;
 }
 
 function PaymentHandler(props: PaymentHandlerProps) {
-	const { userId, order, isOpen, onClose, onSuccess, onFailure } = props;
+  const { userId, order, isOpen, onClose, onSuccess, onFailure } = props;
 
-	const squareAppId = import.meta.env.VITE_SQUARE_APP_ID;
-	const locationId = import.meta.env.VITE_SQUARE_LOCATION_ID;
+  const squareAppId = import.meta.env.VITE_SQUARE_APP_ID;
+  const locationId = import.meta.env.VITE_SQUARE_LOCATION_ID;
 
-	const [tokenSuccess, setTokenSuccess] = useState(false);
+  console.log("squareAppId: ", squareAppId);
+  console.log("locationId: ", locationId);
 
-	const placeOrder = (token: string) => {
-		supabase.functions
-			.invoke("square-api", {
-				body: {
-					name: "Functions",
-					sourceId: token,
-					userId: userId,
-					dueAt: order.due_at,
-					total: order.total,
-					note: order.note,
-					orderItems: order.items.map((item) => ({
-						is_large: item.is_large,
-						menuItemId: item.item.id,
-						quantity: item.quantity,
-						note: item.note,
-						modifiers: item.modifiers?.map((modifier) => modifier.id),
-					})),
-				},
-			})
-			.then((data) => {
-				if (data.data === "ok") {
-					onSuccess();
-				} else {
-					console.error(data.error);
-					onFailure(data.error);
-				}
-			})
-			.catch((error: string) => onFailure(error));
-	};
+  const [tokenSuccess, setTokenSuccess] = useState(false);
 
-	return (
-		<Modal
-			fullScreen
-			opened={isOpen}
-			onClose={onClose}
-			title="Pay securely with Square"
-		>
-			<Stack>
-				<Accordion chevronPosition="left" variant="separated">
-					<Accordion.Item value={"order_summary"}>
-						<Accordion.Control>
-							<Flex justify="space-between">
-								<Text>Show Order Summary</Text>
-								<Text>Total: ${order.total.toFixed(2)}</Text>
-							</Flex>
-						</Accordion.Control>
-						<Accordion.Panel>
-							<OrderSummary order={order} />
-						</Accordion.Panel>
-					</Accordion.Item>
-				</Accordion>
+  const placeOrder = (token: string) => {
+    supabase.functions
+      .invoke("square-api", {
+        body: {
+          name: "Functions",
+          sourceId: token,
+          userId: userId,
+          dueAt: order.due_at,
+          total: order.total,
+          note: order.note,
+          orderItems: order.items.map((item) => ({
+            is_large: item.is_large,
+            menuItemId: item.item.id,
+            quantity: item.quantity,
+            note: item.note,
+            modifiers: item.modifiers?.map((modifier) => modifier.id),
+          })),
+        },
+      })
+      .then((data) => {
+        if (data.data === "ok") {
+          onSuccess();
+        } else {
+          console.error(data.error);
+          onFailure(data.error);
+        }
+      })
+      .catch((error: string) => onFailure(error));
+  };
 
-				{tokenSuccess ? (
-					<Stack align="center">
-						<Text size="lg">Please wait while we process your order</Text>
-						<Loader color="darkslategray" type="dots" />
-					</Stack>
-				) : (
-					<PaymentForm
-						locationId={locationId}
-						applicationId={squareAppId}
-						cardTokenizeResponseReceived={(token, buyer) => {
-							console.info({ token, buyer });
-							if (token.status === "OK") {
-								setTokenSuccess(true);
-								placeOrder(token.token);
-							}
-						}}
-						createPaymentRequest={() => ({
-							countryCode: "AU",
-							currencyCode: "AUD",
-							total: {
-								amount: order.total.toString(),
-								label: "Total",
-							},
-						})}
-					>
-						<Stack gap="md">
-							<Text ta="center">Express Checkout</Text>
+  return (
+    <Modal
+      fullScreen
+      opened={isOpen}
+      onClose={onClose}
+      title="Pay securely with Square"
+    >
+      <Stack>
+        <Accordion chevronPosition="left" variant="separated">
+          <Accordion.Item value={"order_summary"}>
+            <Accordion.Control>
+              <Flex justify="space-between">
+                <Text>Show Order Summary</Text>
+                <Text>Total: ${order.total.toFixed(2)}</Text>
+              </Flex>
+            </Accordion.Control>
+            <Accordion.Panel>
+              <OrderSummary order={order} />
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
 
-							<GooglePay />
+        {tokenSuccess ? (
+          <Stack align="center">
+            <Text size="lg">Please wait while we process your order</Text>
+            <Loader color="darkslategray" type="dots" />
+          </Stack>
+        ) : (
+          <PaymentForm
+            locationId={locationId}
+            applicationId={squareAppId}
+            cardTokenizeResponseReceived={(token, buyer) => {
+              console.info({ token, buyer });
+              if (token.status === "OK") {
+                setTokenSuccess(true);
+                placeOrder(token.token);
+              }
+            }}
+            createPaymentRequest={() => ({
+              countryCode: "AU",
+              currencyCode: "AUD",
+              total: {
+                amount: order.total.toString(),
+                label: "Total",
+              },
+            })}
+          >
+            <Stack gap="md">
+              <Text ta="center">Express Checkout</Text>
 
-							<Divider label="OR" labelPosition="center" />
+              <GooglePay />
 
-							<CreditCard includeInputLabels />
-						</Stack>
-					</PaymentForm>
-				)}
-			</Stack>
-		</Modal>
-	);
+              <Divider label="OR" labelPosition="center" />
+
+              <CreditCard includeInputLabels />
+            </Stack>
+          </PaymentForm>
+        )}
+      </Stack>
+    </Modal>
+  );
 }
 
 export default PaymentHandler;
