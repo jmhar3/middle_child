77c496e151966edd3549868e4329d59eef2edc55
jmhar3 <jmhar@protonmail.com>
2026-08-06 15:31:15 +1000

    hide account components if no user

--- src/pages/customer/Account.tsx
+++ src/pages/customer/Account.tsx
@@ -71,91 +71,91 @@ function Account() {
 					onModalClose={() => (user ? navigate("/account") : navigate("/"))}
 				/>
 			)}
-			<Stack w="100%" p="sm">
-				<StyledButton
-					label="Sign Out"
-					onClick={onSignOut}
-					isDisabled={isLoading}
-				/>
-
-				<Divider />
-
-				<Stack gap="sm">
-					<TextInput
-						w="100%"
-						size="md"
-						value={name}
-						label="Update Name"
-						onChange={(e) => setName(e.target.value)}
-					/>
-
+			{user && (
+				<Stack w="100%" p="sm">
 					<StyledButton
-						label="Update Name"
-						onClick={onUpdateUserName}
+						label="Sign Out"
+						onClick={onSignOut}
 						isDisabled={isLoading}
 					/>
+
+					<Divider />
+
+					<Stack gap="sm">
+						<TextInput
+							w="100%"
+							size="md"
+							value={name}
+							label="Update Name"
+							onChange={(e) => setName(e.target.value)}
+						/>
+
+						<StyledButton
+							label="Update Name"
+							onClick={onUpdateUserName}
+							isDisabled={isLoading}
+						/>
+					</Stack>
+
+					<Divider />
+
+					<LoyaltyPoints />
+
+					{userOrders?.length && userOrders.length > 0 && (
+						<>
+							<Divider />
+
+							<Stack bd="solid 1px darkslategray" bdrs="sm" gap="0" p="sm">
+								<Text>Order History</Text>
+
+								<Accordion
+									styles={{
+										item: { borderColor: "darkslategray" },
+										content: {
+											padding: 0,
+											margin: 0,
+											backgroundColor: "white",
+										},
+										control: {
+											backgroundColor: "whitesmoke",
+										},
+									}}
+								>
+									{userOrders.map((order) => (
+										<Accordion.Item key={order.id} value={order.id}>
+											<Accordion.Control>
+												<Stack component="span">
+													<Flex>
+														<Stack gap="0">
+															<Text>
+																{dayjs(order.due_at).format("MMM D h:mma")}
+															</Text>
+															<Text size="xs">
+																{order.items
+																	.map(({ quantity }) => quantity)
+																	.reduce(
+																		(accumulator, currentValue) =>
+																			accumulator + currentValue,
+																		0,
+																	)}{" "}
+																items |{" "}
+																{order.is_complete ? "Complete" : "In Progress"}
+															</Text>
+														</Stack>
+													</Flex>
+												</Stack>
+											</Accordion.Control>
+											<Accordion.Panel>
+												<OrderSummary order={order} />
+											</Accordion.Panel>
+										</Accordion.Item>
+									))}
+								</Accordion>
+							</Stack>
+						</>
+					)}
 				</Stack>
-
-				<Divider />
-
-				<LoyaltyPoints />
-
-				{userOrders?.length && userOrders.length > 0 ? (
-					<>
-						<Divider />
-
-						<Stack bd="solid 1px darkslategray" bdrs="sm" gap="0" p="sm">
-							<Text>Order History</Text>
-
-							<Accordion
-								styles={{
-									item: { borderColor: "darkslategray" },
-									content: {
-										padding: 0,
-										margin: 0,
-										backgroundColor: "white",
-									},
-									control: {
-										backgroundColor: "whitesmoke",
-									},
-								}}
-							>
-								{userOrders.map((order) => (
-									<Accordion.Item key={order.id} value={order.id}>
-										<Accordion.Control>
-											<Stack component="span">
-												<Flex>
-													<Stack gap="0">
-														<Text>
-															{dayjs(order.due_at).format("MMM D h:mma")}
-														</Text>
-														<Text size="xs">
-															{order.items
-																.map(({ quantity }) => quantity)
-																.reduce(
-																	(accumulator, currentValue) =>
-																		accumulator + currentValue,
-																	0,
-																)}{" "}
-															items |{" "}
-															{order.is_complete ? "Complete" : "In Progress"}
-														</Text>
-													</Stack>
-												</Flex>
-											</Stack>
-										</Accordion.Control>
-										<Accordion.Panel>
-											<OrderSummary order={order} />
-										</Accordion.Panel>
-									</Accordion.Item>
-								))}
-							</Accordion>
-						</Stack>
-					</>
-				) : (
-					<></>
-				)}
-			</Stack>
+			)}
 		</PageLayout>
 	);
 }
